c1f08149646a37d00b304badb1f15d976b089115
vaibhav singh <vaibhav0001singh@gmail.com>
2024-03-20 17:26:12 +0530

    .

--- SCRATCH-WEB-SERVER/go.mod
+++ SCRATCH-WEB-SERVER/go.mod
@@ -1,3 +1,5 @@
 module SCRATCH-WEB-SERVER
 
 go 1.22.0
+
+require github.com/joho/godotenv v1.5.1
--- SCRATCH-WEB-SERVER/main.go
+++ SCRATCH-WEB-SERVER/main.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println()
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
