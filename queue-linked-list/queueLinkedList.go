ff09edcf554e956140a3b77e7a91c5fa65d1d043
vaibhav singh <vaibhav0001singh@gmail.com>
2024-03-26 14:07:15 +0530

    .

--- queue-linked-list/queueLinkedList.go
+++ queue-linked-list/queueLinkedList.go
@@ -1,3 +1,24 @@
+/*package main
+import "container/list"
+import "fmt"
+
+func main() {
+	// new linked list
+	queue := list.New()
+
+	// Simply append to enqueue.
+	queue.PushBack(10)
+	queue.PushBack(20)
+	queue.PushBack(30)
+
+	// Dequeue
+	front:=queue.Front()
+	fmt.Println(front.Value)
+	queue.Remove(front)
+}
+Linked list implementation for queue.
+*/
+
 package main
 
 import (
